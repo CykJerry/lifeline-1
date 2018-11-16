b2b8dd5f4f0e8c8b0bb4154c4ec24cde5ed884c3
xuesui <331063594@qq.com>
2018-11-16 12:46:18 +0800

    第三次作业

---
+++ lifeline/src/com/jude/prisoner/XsPrisoner.java
@@ -0,0 +1,31 @@
+package com.jude.prisoner;
+
+import com.jude.Manager;
+import com.jude.Prisoner;
+
+public class XsPrisoner implements Prisoner {
+    private int totalCount;
+    private int totalPerson;
+
+    @Override
+    public String getName() {
+        return "梁瀚2018214044";
+    }
+
+    @Override
+    public void begin(Manager manager, int totalPerson, int totalCount) {
+        this.totalCount = totalCount;
+        this.totalPerson = totalPerson;
+    }
+
+    @Override
+    public int take(int index, int last) {
+        return ((totalCount - last) / (index + 1) + last / totalPerson);
+    }
+
+    @Override
+    public void result(boolean survived) {
+
+    }
+
+}
